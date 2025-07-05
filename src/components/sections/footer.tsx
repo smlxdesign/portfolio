e5a70c30615c0d70471d733e467af22de310e0ca
Samuel <samuel.smlxdesign@gmail.com>
2025-07-05 14:20:04 +0200

    feat: add scroll to top button

--- src/components/sections/footer.tsx
+++ src/components/sections/footer.tsx
@@ -1,5 +1,6 @@
 import {
 	type RemixiconComponentType,
+	RiArrowUpLine,
 	RiBlueskyLine,
 	RiBox3Line,
 	RiChat3Line,
@@ -50,107 +51,112 @@ export function Footer() {
 	}
 
 	return (
-		<section className="flex flex-col gap-16 px-responsive py-responsive md:flex-row md:justify-between lg:gap-20 xl:gap-32">
-			<Image src={logo} alt="SMLX Design Logo" className="size-48" />
-			<div className="flex flex-col gap-10 sm:flex-row sm:flex-wrap sm:gap-20">
-				<FooterList
-					category="Jump to..."
-					links={[
-						{
-							icon: RiHomeLine,
-							title: "Home",
-							href: "/",
-						},
-						{
-							icon: RiBox3Line,
-							title: "Products",
-							href: "/products/",
-						},
-						{
-							icon: RiEditLine,
-							title: "Blog",
-							href: "/blog/",
-						},
-						{
-							icon: RiContactsBookLine,
-							title: "Contact",
-							href: "/contact",
-						},
-					]}
-				/>
-				<FooterList
-					category="Products"
-					links={products
-						.filter((_, index) => index < 3)
-						.map((product) => {
-							return {
-								icon: product.icon ?? RiBox3Line,
-								title: product.title,
-								href: product.links[0]?.href ?? "/products/",
-							};
-						})}
-				/>
-				<FooterList
-					category="Socials"
-					links={[
-						{
-							icon: RiGithubLine,
-							title: "Github",
-							href: "https://github.com/smlxdesign",
-						},
-						{
-							icon: RiTwitterLine,
-							title: "Twitter/X",
-							href: "https://x.com/realsmlxdesign",
-						},
-						{
-							icon: RiBlueskyLine,
-							title: "Bluesky",
-							href: "https://bsky.app/profile/smlxdesign.bsky.social",
-						},
-						{
-							icon: RiMastodonLine,
-							title: "Mastodon",
-							href: "https://mastodon.social/@smlxdesign",
-						},
-					]}
-				/>
-				<FooterList
-					category="Contact"
-					links={[
-						{
-							icon: RiMailUnreadLine,
-							title: "Send an Email",
-							href: "mailto:samuel.smlxdesign@gmail.com",
-						},
-						{
-							icon: RiChat3Line,
-							title: "DM on Twitter",
-							href: "https://x.com/realsmlxdesign",
-						},
-						{
-							icon: RiSurveyLine,
-							title: "Contact Form",
-							href: "/contact",
-						},
-					]}
-				/>
-				<FooterList
-					category="This Site"
-					links={[
-						{
-							icon: RiCodeSLine,
-							title: "Source Code",
-							href: "https://github.com/smlxdesign/portfolio",
-						},
-						{
-							icon: RiCopyrightLine,
-							title: "License",
-							href: "https://github.com/smlxdesign/portfolio/blob/main/LICENSE",
-						},
-					]}
-				/>
+		<section className="flex flex-col items-center gap-16 px-responsive py-responsive">
+			<div className="flex flex-col gap-16 md:flex-row md:justify-between lg:gap-20 xl:gap-32">
+				<Image src={logo} alt="SMLX Design Logo" className="size-48" />
+				<div className="flex flex-col gap-10 sm:flex-row sm:flex-wrap sm:gap-20">
+					<FooterList
+						category="Jump to..."
+						links={[
+							{
+								icon: RiHomeLine,
+								title: "Home",
+								href: "/",
+							},
+							{
+								icon: RiBox3Line,
+								title: "Products",
+								href: "/products/",
+							},
+							{
+								icon: RiEditLine,
+								title: "Blog",
+								href: "/blog/",
+							},
+							{
+								icon: RiContactsBookLine,
+								title: "Contact",
+								href: "/contact",
+							},
+						]}
+					/>
+					<FooterList
+						category="Products"
+						links={products
+							.filter((_, index) => index < 3)
+							.map((product) => {
+								return {
+									icon: product.icon ?? RiBox3Line,
+									title: product.title,
+									href: product.links[0]?.href ?? "/products/",
+								};
+							})}
+					/>
+					<FooterList
+						category="Socials"
+						links={[
+							{
+								icon: RiGithubLine,
+								title: "Github",
+								href: "https://github.com/smlxdesign",
+							},
+							{
+								icon: RiTwitterLine,
+								title: "Twitter/X",
+								href: "https://x.com/realsmlxdesign",
+							},
+							{
+								icon: RiBlueskyLine,
+								title: "Bluesky",
+								href: "https://bsky.app/profile/smlxdesign.bsky.social",
+							},
+							{
+								icon: RiMastodonLine,
+								title: "Mastodon",
+								href: "https://mastodon.social/@smlxdesign",
+							},
+						]}
+					/>
+					<FooterList
+						category="Contact"
+						links={[
+							{
+								icon: RiMailUnreadLine,
+								title: "Send an Email",
+								href: "mailto:samuel.smlxdesign@gmail.com",
+							},
+							{
+								icon: RiChat3Line,
+								title: "DM on Twitter",
+								href: "https://x.com/realsmlxdesign",
+							},
+							{
+								icon: RiSurveyLine,
+								title: "Contact Form",
+								href: "/contact",
+							},
+						]}
+					/>
+					<FooterList
+						category="This Site"
+						links={[
+							{
+								icon: RiCodeSLine,
+								title: "Source Code",
+								href: "https://github.com/smlxdesign/portfolio",
+							},
+							{
+								icon: RiCopyrightLine,
+								title: "License",
+								href: "https://github.com/smlxdesign/portfolio/blob/main/LICENSE",
+							},
+						]}
+					/>
+				</div>
 			</div>
+			<Link href="#" className="flex items-center gap-2">
+				<RiArrowUpLine className="size-4" /> Scroll to the top
+			</Link>
 		</section>
 	);
 }
