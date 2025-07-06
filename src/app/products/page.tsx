64e15e287eeb51ca9a1f5afeb24447d59d398e7e
Samuel <samuel.smlxdesign@gmail.com>
2025-07-06 20:23:26 +0200

    refactor: move "teaser" into its own component
    I honestly don't know what this component is. I would call it card, but
    there are other cards present (shadcn/ui) which does not look like this
    in the slightest.

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,14 +1,9 @@
-import Image from "next/image";
-import Link from "next/link";
 import { Navbar } from "~/components/navbar";
 import { Contact } from "~/components/sections/contact";
 import { Footer } from "~/components/sections/footer";
 import { Hero } from "~/components/sections/hero";
-import { Heading } from "~/components/typography/heading";
-import { Badge } from "~/components/ui/badge";
-import { Button } from "~/components/ui/button";
+import { Teaser } from "~/components/teaser";
 import { products } from "~/data/products";
-import { tags } from "~/data/tags";
 
 export default function Page() {
 	return (
@@ -18,51 +13,8 @@ export default function Page() {
 				<Hero>Products</Hero>
 				<ul className="flex flex-col gap-24 px-responsive py-responsive">
 					{products.map((product) => (
-						<li key={product.id} className="flex flex-col gap-16 md:flex-row">
-							<Image
-								src={product.image.href}
-								alt={product.image.alt ?? ""}
-								width={1920}
-								height={1080}
-								className="w-full rounded-xl"
-							/>
-							<div className="flex w-full flex-col justify-between">
-								<div className="flex flex-col gap-3">
-									<Heading>{product.title}</Heading>
-									<div className="flex flex-wrap gap-2">
-										{product.tags.map((tagId) => {
-											const tag = tags[tagId];
-											console.log(tag);
-
-											if (tag === undefined) {
-												return null;
-											}
-
-											return (
-												<Badge variant="secondary" key={tag.title}>
-													<tag.icon />
-													{tag.title}
-												</Badge>
-											);
-										})}
-									</div>
-									<p className="max-w-[65ch]">{product.description}</p>
-								</div>
-								<div className="flex gap-2">
-									{product.links.map((link, index) => (
-										<Button
-											key={link.href}
-											asChild
-											variant={index === 0 ? "default" : "secondary"}
-										>
-											<Link href={link.href}>
-												<link.icon />
-												{link.title}
-											</Link>
-										</Button>
-									))}
-								</div>
-							</div>
+						<li key={product.id}>
+							<Teaser {...product} />
 						</li>
 					))}
 				</ul>
---
+++ src/components/teaser.tsx
@@ -0,0 +1,68 @@
+import type { RemixiconComponentType } from "@remixicon/react";
+import Image from "next/image";
+import Link from "next/link";
+import { tags } from "~/data/tags";
+import { Heading } from "./typography/heading";
+import { Badge } from "./ui/badge";
+import { Button } from "./ui/button";
+
+export function Teaser(props: {
+	image: { href: string; alt?: string };
+	title: string;
+	description?: string;
+	tags: string[];
+	links: Array<{
+		href: string;
+		title: string;
+		icon: RemixiconComponentType;
+	}>;
+}) {
+	return (
+		<div className="flex flex-col gap-16 md:flex-row">
+			<Image
+				src={props.image.href}
+				alt={props.image.alt ?? ""}
+				width={1920}
+				height={1080}
+				className="w-full rounded-xl"
+			/>
+			<div className="flex w-full flex-col justify-between">
+				<div className="flex flex-col gap-3">
+					<Heading>{props.title}</Heading>
+					<div className="flex flex-wrap gap-2">
+						{props.tags.map((tagId) => {
+							const tag = tags[tagId];
+							console.log(tag);
+
+							if (tag === undefined) {
+								return null;
+							}
+
+							return (
+								<Badge variant="secondary" key={tag.title}>
+									<tag.icon />
+									{tag.title}
+								</Badge>
+							);
+						})}
+					</div>
+					<p className="max-w-[65ch]">{props.description}</p>
+				</div>
+				<div className="flex gap-2">
+					{props.links.map((link, index) => (
+						<Button
+							key={link.href}
+							asChild
+							variant={index === 0 ? "default" : "secondary"}
+						>
+							<Link href={link.href}>
+								<link.icon />
+								{link.title}
+							</Link>
+						</Button>
+					))}
+				</div>
+			</div>
+		</div>
+	);
+}
