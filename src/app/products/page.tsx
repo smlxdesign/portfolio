1d2e53495d22315c9c25a23671d9b5641e170e5c
Samuel <samuel.smlxdesign@gmail.com>
2025-07-06 15:33:35 +0200

    feat: create products page

---
+++ src/app/products/page.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Navbar } from "~/components/navbar";
+import { Hero } from "~/components/sections/hero";
+import { Heading } from "~/components/typography/heading";
+import { Button } from "~/components/ui/button";
+import { products } from "~/data/products";
+
+export default function Page() {
+	return (
+		<div className="flex flex-col">
+			<Navbar />
+			<main>
+				<Hero>Products</Hero>
+				<ul className="flex flex-col gap-24 px-responsive py-responsive">
+					{products.map((product) => (
+						<li key={product.id} className="flex flex-col gap-16 md:flex-row">
+							<Image
+								src={product.image.href}
+								alt={product.image.alt ?? ""}
+								width={1920}
+								height={1080}
+								className="w-full rounded-xl"
+							/>
+							<div className="flex w-full flex-col justify-between">
+								<div className="flex flex-col gap-3">
+									<Heading>{product.title}</Heading>
+									<p className="max-w-[65ch]">{product.description}</p>
+								</div>
+								<div className="flex gap-2">
+									{product.links.map((link, index) => (
+										<Button
+											key={link.href}
+											asChild
+											variant={index === 0 ? "default" : "secondary"}
+										>
+											<Link href={link.href}>
+												{link.icon}
+												{link.title}
+											</Link>
+										</Button>
+									))}
+								</div>
+							</div>
+						</li>
+					))}
+				</ul>
+			</main>
+		</div>
+	);
+}
