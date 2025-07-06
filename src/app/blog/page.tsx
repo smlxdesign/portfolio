3fef64da5520a3f441611b1cf87f8874c3401064
Samuel <samuel.smlxdesign@gmail.com>
2025-07-06 19:16:27 +0200

    feat: create blog page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,11 @@
+import { Navbar } from "~/components/navbar";
+
+export default function Page() {
+	return (
+		<div className="flex flex-col">
+			<Navbar />
+			<main></main>
+		</div>
+	);
+}
+;
