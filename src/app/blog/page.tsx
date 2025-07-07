66f8aedd26164295945916fc595a945ac2110a57
Samuel <samuel.smlxdesign@gmail.com>
2025-07-07 12:15:18 +0200

    feat: list blog posts

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,14 +1,35 @@
+import { RiNewsLine } from "@remixicon/react";
 import { Navbar } from "~/components/navbar";
 import { Contact } from "~/components/sections/contact";
-import { Hero } from "~/components/sections/hero";
 import { Footer } from "~/components/sections/footer";
+import { Hero } from "~/components/sections/hero";
+import { Teaser } from "~/components/teaser";
+import { getContentData } from "~/data/content";
 
 export default function Page() {
+	const content = getContentData();
+
 	return (
 		<div className="flex flex-col">
 			<Navbar />
 			<main>
 				<Hero>Blog</Hero>
+				<ul className="flex flex-col gap-24 px-responsive py-responsive">
+					{content.map((post) => (
+						<li key={post.id}>
+							<Teaser
+								links={[
+									{
+										title: "Read",
+										href: `/blog/ + ${post.id}`,
+										icon: RiNewsLine,
+									},
+								]}
+								{...post}
+							/>
+						</li>
+					))}
+				</ul>
 			</main>
 			<footer>
 				<Contact />
