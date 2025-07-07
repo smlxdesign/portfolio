989f46df3f43b0a9b41575f2bba0cea5d66afa0a
Samuel <samuel.smlxdesign@gmail.com>
2025-07-07 12:14:25 +0200

    feat: validate frontmatter with zod schema

--- src/data/content.ts
+++ src/data/content.ts
@@ -1,6 +1,19 @@
-import matter from "gray-matter";
+import { readFileSync, readdirSync } from "node:fs";
 import path from "node:path";
-import { readdirSync, readFileSync } from "node:fs";
+import matter from "gray-matter";
+import { z } from "zod/v4";
+
+export const schema = z.object({
+	id: z.string().min(3),
+	title: z.string().min(3),
+	description: z.string().optional(),
+	image: z.object({
+		href: z.string(),
+		alt: z.string().optional(),
+	}),
+	date: z.iso.date().transform((str) => new Date(str)),
+	tags: z.array(z.string()),
+});
 
 const contentDirectory = path.join(process.cwd(), "src", "content");
 
@@ -8,17 +21,19 @@ export function getContentData() {
 	const filenames = readdirSync(contentDirectory);
 	const contentData = filenames.map((filename) => {
 		const id = filename.replace(/\.(md|mdx)$/, "");
+		console.log(`id: ${id}`);
 
 		const fullPath = path.join(contentDirectory, filename);
 		const fileContent = readFileSync(fullPath, "utf-8");
+		console.log(fullPath, fileContent);
 
 		const frontmatter = matter(fileContent);
+		console.log(frontmatter);
 
 		return {
 			id,
 			...frontmatter.data,
 		};
 	});
-
-	return contentData;
+	return z.array(schema).parse(contentData);
 }
