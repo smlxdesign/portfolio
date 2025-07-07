5a903c11814e03899487f83f5730f8fd1c081d59
Samuel <samuel.smlxdesign@gmail.com>
2025-07-07 11:21:17 +0200

    feat: add title and footer on blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,11 +1,19 @@
 import { Navbar } from "~/components/navbar";
+import { Contact } from "~/components/sections/contact";
+import { Hero } from "~/components/sections/hero";
+import { Footer } from "~/components/sections/footer";
 
 export default function Page() {
 	return (
 		<div className="flex flex-col">
 			<Navbar />
-			<main></main>
+			<main>
+				<Hero>Blog</Hero>
+			</main>
+			<footer>
+				<Contact />
+				<Footer />
+			</footer>
 		</div>
 	);
 }
-;
