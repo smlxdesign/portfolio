4c1499afcdb1680cf15e3d63675db9df40895d86
Samuel <samuel.smlxdesign@gmail.com>
2025-07-08 12:36:53 +0200

    fix: make contact page responsive

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,16 +3,16 @@ import { Heading } from "~/components/typography/heading";
 
 export default function Page() {
 	return (
-		<section className="grid min-h-lvh grid-cols-2 gap-16 bg-[url(/contact-background.webp))] px-responsive py-responsive">
-			<div className="flex flex-col items-center justify-center gap-8">
-				<div className="flex flex-col gap-2">
+		<section className="flex min-h-lvh flex-col gap-16 bg-[url(/contact-background.webp))] px-responsive py-responsive lg:grid lg:grid-cols-2">
+			<div className="z-10 mt-20 flex flex-col items-center justify-center gap-8">
+				<div className="flex flex-col gap-2 max-w-100">
 					<Heading>Email</Heading>
 					<p>
 						Send an email to samuel.smlxdesign@gmail.com using you preferred
 						mailing client and wait for a response.
 					</p>
 				</div>
-				<div className="flex flex-col gap-2">
+				<div className="flex flex-col gap-2 max-w-100">
 					<Heading>Other Platforms</Heading>
 					<p>
 						I have an account on multiple platforms. On Twitter/X you can find
