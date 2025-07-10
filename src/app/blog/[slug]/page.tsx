56fe2275fbdefcf874eee2135e0ad068167fa636
Samuel <samuel.smlxdesign@gmail.com>
2025-07-10 13:39:30 +0200

    feat: add second column

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,7 +1,12 @@
+import { RiFacebookCircleLine, RiTwitterLine } from "@remixicon/react";
 import Image from "next/image";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 import { Heading } from "~/components/typography/heading";
+import { Badge } from "~/components/ui/badge";
+import { Button } from "~/components/ui/button";
 import { getContentData, getPostById } from "~/data/content";
+import { tags } from "~/data/tags";
 
 export async function generateStaticParams() {
 	const posts = await getContentData();
@@ -29,8 +34,24 @@ export default async function Page({
 				height={1080}
 				className="aspect-[4/3] w-full object-cover sm:aspect-[12/5]"
 			/>
-			<section className="flex flex-col px-responsive lg:flex-row-reverse">
-				<div>
+			<section className="flex min-h-lvh flex-col gap-16 px-responsive lg:flex-row-reverse">
+				<div className="sticky top-0 flex min-h-lvh w-full flex-col gap-4 py-responsive">
+					{post.description && <p>{post.description}</p>}
+					<div className="flex flex-wrap gap-2">
+						{post.tags.map((tagId) => {
+							const tag = tags[tagId];
+
+							if (!tag) return null;
+
+							return (
+								<Badge key={tagId} variant="secondary">
+									<tag.icon /> {tag.title}
+								</Badge>
+							);
+						})}
+					</div>
+				</div>
+				<div className="w-full py-responsive">
 					<Heading>{post.title}</Heading>
 					{/**
 					 *	biome-ignore lint/security/noDangerouslySetInnerHtml: the docs told me to do this
@@ -38,7 +59,6 @@ export default async function Page({
 					 */}
 					<div dangerouslySetInnerHTML={{ __html: post.content }} />
 				</div>
-				<div></div>
 			</section>
 		</>
 	);
