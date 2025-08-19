feb31107bfee0320568ad6db6479a4c2b28d7625
Samuel <samuel.smlxdesign@gmail.com>
2025-08-19 15:18:28 +0200

    feat: add sad smiley on 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,10 +2,17 @@ import { RiHomeLine } from "@remixicon/react";
 import Link from "next/link";
 import { Title } from "~/components/typography/title";
 import { Button } from "~/components/ui/button";
+import notfoundSadVector from "~/assets/404-sad.svg";
+import Image from "next/image";
 
 export default function Page() {
 	return (
 		<section className="mt-20 flex flex-col gap-8 px-responsive py-responsive">
+			<Image
+				className="xl:absolute xl:right-10 scale-75 xl:right-40"
+				src={notfoundSadVector}
+				alt=""
+			/>
 			<Title className="max-w-lg">This site does not exist :/</Title>
 			<p className="max-w-lg">
 				Error 404: Not found. You may have mistyped the url, or this page may
