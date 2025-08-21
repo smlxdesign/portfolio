fff914bb2be97488989e6fee45509bdf5c92e468
Samuel <samuel.smlxdesign@gmail.com>
2025-08-21 20:53:39 +0200

    feat: remove tracking code

--- src/instrumentation-client.ts
+++
@@ -1,7 +0,0 @@
-import posthog from "posthog-js";
-import { env } from "./env.js";
-
-posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
-	api_host: env.NEXT_PUBLIC_POSTHOG_HOST,
-	defaults: "2025-05-24",
-});
