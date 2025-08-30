7b788f558ee66a0477e4606b03d0b16ee0ab4045
Samuel <samuel.smlxdesign@gmail.com>
2025-08-30 17:25:41 +0200

    feat: add opengraph metadata to blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,8 +1,21 @@
+import type { Metadata } from "next";
 import { RiNewsLine } from "@remixicon/react";
 import { format } from "date-fns";
 import { Hero } from "~/components/sections/hero";
 import { Teaser } from "~/components/teaser";
 import { getContentData } from "~/data/content";
+
+export const metadata: Metadata = {
+	title: "Blog",
+	openGraph: {
+		title: "Blog",
+		description: "A list of my blog posts",
+	},
+	twitter: {
+		title: "Blog",
+		description: "A list of my blog posts",
+	},
+};
 
 export default async function Page() {
 	const content = await getContentData();
